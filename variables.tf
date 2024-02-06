d81a2c1a9a44d7a979965d9270f1db48b1992b3d
Rishi <154218027+rishi-hashicorp@users.noreply.github.com>
2024-02-06 09:38:54 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,25 +1,9 @@
 variable "region" {
-  description = "GCP region"
-  default     = "us-central1"
+  description = "AWS region"
+  default     = "us-east-1"
 }
 
-variable "zone" {
-  description = "GCP zone"
-  default     = "us-central1-a"
-}
-
-variable "project" {
-  description = "GCP Project ID"
-  default     = "hc-0ea6eeb77ca64b1896879483e05"
-}
-
-variable "machine_type" {
+variable "instancetype" {
   description = "Type of VM instance to provision"
-  default     = "e2-micro"
+  default     = "t2.micro"
 }
-
-variable "vm_name" {
-  description = "VM instance name"
-  default     = "demoinstance-vcs"
-}
-
