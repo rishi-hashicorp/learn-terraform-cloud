e1241d3d1b0d359865f650be802f0625414d05c3
Rishi <154218027+rishi-hashicorp@users.noreply.github.com>
2024-02-13 15:07:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,3 +19,15 @@ resource "aws_instance" "app_server" {
     Name = "ExampleAppServerInstance"
   }
 }
+
+check "health_check" {
+  data "http" "terraform_io" {
+    url = "https://www.terraform.io"
+  }
+
+  assert {
+    condition = data.http.terraform_io.status_code == 200
+    error_message = "${data.http.terraform_io.url} returned an unhealthy status code"
+  }
+}
+
