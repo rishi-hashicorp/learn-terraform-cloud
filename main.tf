e51227924c709428c348a35ba9b7a289bb68d37c
Rishi <154218027+rishi-hashicorp@users.noreply.github.com>
2024-03-21 11:06:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,13 +7,37 @@ terraform {
   }
 }
 
+data "aws_ami" "demo" {
+  owners = ["amazon"]
+
+  filter {
+    name   = "image-id"
+    values = ["ami-0277155c3f0ab2930"]
+  }
+}
+
 provider "aws" {
   region  = var.region
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0277155c3f0ab2930"
+  ami           = data.aws_ami.demo.id
   instance_type = var.instancetype
+
+  lifecycle {
+    # The AMI ID must refer to an AMI that contains an operating system
+    # for the `x86_64` architecture.
+    precondition {
+      condition     = data.aws_ami.demo.architecture == "x86_64"
+      error_message = "The selected AMI must be for the x86_64 architecture."
+    }
+
+    postcondition {
+      condition     = self.public_dns != ""
+      error_message = "EC2 instance must be in a VPC that has public DNS hostnames enabled."
+    }
+  }
+}
 
   tags = {
     Name = "ExampleAppServerInstance"
